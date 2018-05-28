e910cb81c2c632f2a32afbba2dfa0474d7c3b0f4
Josephine <josephinewliu@gmail.com>
2018-05-28 14:58:59 -0700

    added scale definitons & edited targeted audience message

--- bf_project/ui.R
+++ bf_project/ui.R
@@ -13,14 +13,34 @@ shinyUI(navbarPage(
       "from ", a(href = "https://data.seattle.gov", "City of Seattle Open Data Portal"),
       "."
     ),
-    h1("Audience"),
-    p("While anybody could get some insights from this report, our intended audience",
-      "for this report consists of those who are interested in investing in real estate,",
-      "moving to a new neighborhood or starting businesses, such as running a restaurant.",
-      "They could get a broad overview of the frequencies of the crimes happened lately in",
-      "each neighborhood, which would be able to help them make decisions."
+    h1("Audience & Intended Takeaways"),
+    p("Our intended audience for this report consists of those who are interested 
+      in investing in real estate, moving to a new neighborhood or starting 
+      businesses in the general Seattle area. Investors can get a broad overview 
+      of the frequencies of the crimes, as well as type of crimes commonly found
+      in each neighborhood, aiding in critical decisions. However, our targeted 
+      audience should not limit the message conveyed by this report, as the
+      correlation often found together with crime and inequities is a not an 
+      isolated issue unique to Seattle."
     )
-  ),
+    ),
+  h1("Organization of Threat Levels"),
+  p("While working with the data set, we decided to categorize label the 
+    featured type of crimes based on how violet or severe they were. Below is a
+    key and loose definitions to describe how we categorized our 'levels' of 
+    crime:
+    - Violent crime = other individuals are generally physically injured or 
+    affected by the crime
+    - Non-violent crime = other individuals are NOT generally physically 
+    injured or affected by the crime
+    
+    - Low impact = generally no harm to individuals or minimal detrimental 
+    affects
+    - Medium impact = potential danger to victim enough to be life-threatening 
+    - High impact = large levels of harm to victims and has lasting detrimental
+    affects"
+  )
+),
   # Create a tab panel for population on state level
   tabPanel(
     "Map",
