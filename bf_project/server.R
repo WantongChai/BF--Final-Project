eed717f058399672f51902d0507bd1d816a63982
Andrew Gies <andrewgies17@gmail.com>
2018-05-28 14:28:35 -0700

    More data wrangling, add funding data.

--- bf_project/server.R
+++ bf_project/server.R
@@ -4,41 +4,74 @@ library(dplyr)
 library(lubridate)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    ## DATA PREP!
-    data <- read.csv("../data/Crime_Data.csv", stringsAsFactors = FALSE)
-
+    ## Crime Data Prep
+    crime_data <-
+        read.csv("../data/Crime_Data.csv", stringsAsFactors = FALSE)
     # Remove UID, not particularly useful for our purposes.
-    data <- data %>% select(-Report.Number)
-
+    crime_data <- crime_data %>% select(-Report.Number)
     # Isolate and Remove Outliers (general data cleanup).
-    beat_outliers <- data %>%
+    beat_outliers <- crime_data %>%
         group_by(Beat) %>%
         summarize(n = n()) %>%
         filter(n <= 10) %>%
         pull(Beat)
-    data <- data %>%
+    crime_data <- crime_data %>%
         filter(!(Beat %in% beat_outliers))
-
-    precinct_outliers <- data %>%
-        group_by(Precinct) %>%
-        summarize(n = n()) %>%
-        filter(n <= 10) %>%
-        pull(Precinct)
-    data <- data %>%
-        filter(!(Precinct %in% precinct_outliers))
-
-    # Null Transforms
-    data <- data %>%
-        mutate(Sector = ifelse(Sector == "", "UNKNOWN", Sector),
-               Beat = ifelse(Beat == "", "UNKNOWN", Beat))
-
+    crime_data <- crime_data %>%
+        filter(
+            Precinct != "UNKNOWN" &
+                Sector != "" &
+                Beat != "" &
+                Neighborhood != "UNKNOWN" &
+                Precinct != ""
+        )
     # Date/Time Transforms
-    data$Occured.Date = mdy(data$Occured.Date)
-    data$Occured.Time = hours(floor(data$Occured.Time / 100)) +
-        minutes(data$Occured.Time %% 100)
-    data$Reported.Date = mdy(data$Reported.Date)
-    data$Reported.Time = hours(floor(data$Reported.Time / 100)) +
-        minutes(data$Reported.Time %% 100)
+    crime_data$Occured.Date = mdy(crime_data$Occured.Date)
+    crime_data$Occured.Time = hours(floor(crime_data$Occured.Time / 100)) +
+        minutes(crime_data$Occured.Time %% 100)
+    crime_data$Reported.Date = mdy(crime_data$Reported.Date)
+    crime_data$Reported.Time = hours(floor(crime_data$Reported.Time / 100)) +
+        minutes(crime_data$Reported.Time %% 100)
+    # Subset Data to Dates of Interest
+    crime_data <- crime_data %>%
+        filter(Occured.Date >= mdy("1-1-2010"))
+    ## Crime Data Prep Complete!
 
-    ## Data Prep Complete!
+    ## Funding Data Prep
+    funding_data <-
+        read.csv("../data/matching_funds.csv", stringsAsFactors = FALSE)
+    # Mapping Funding Districts to Crime Precincts
+    district_map = list(
+        west = c("Downtown", "Magnolia/QA", "Magnolia / Queen Anne"),
+        southwest = c("Southwest", "Delridge"),
+        east = c("East"),
+        north = c("Northeast", "Ballard", "Lake Union", "Northwest", "North"),
+        south = c("Southeast", "Greater Duwamish")
+    )
+    # Beware, this code sorts into precincts.
+    funding_data <- funding_data %>%
+        filter(Award.Year >= 2010) %>%
+        mutate(precinct =
+                   ifelse(
+                       Neighborhood.District %in% district_map$west,
+                       "WEST",
+                       ifelse(
+                           Neighborhood.District %in% district_map$southwest,
+                           "SOUTHWEST",
+                           ifelse(
+                               Neighborhood.District %in% district_map$east,
+                               "EAST",
+                               ifelse(
+                                   Neighborhood.District %in% district_map$north,
+                                   "NORTH",
+                                   ifelse(
+                                       Neighborhood.District %in% district_map$south,
+                                       "SOUTH",
+                                       "UNKNOWN"
+                                   )
+                               )
+                           )
+                       )
+                   )) %>%
+        filter(precinct != "UNKNOWN")
 })
