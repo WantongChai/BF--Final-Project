601ca084ec1ee7d164f5f5d496580293d9bdb8d8
Josephine <josephinewliu@gmail.com>
2018-05-30 13:27:05 -0700

    bare + lots of bugs

--- scripts/crime_month.R
+++ scripts/crime_month.R
@@ -1,15 +1,18 @@
 library(plotly)
 library(dplyr)
 
+
 build_line <- function(dataset, crime) {
-  p <- plot_ly(data = , x = ~month, y = ~high_2014, name = 'High 2014', type = 'scatter', mode = 'lines',
-               line = list(color = 'rgb(205, 12, 24)', width = 4)) %>%
-    add_trace(y = ~low_2014, name = 'Low 2014', line = list(color = 'rgb(22, 96, 167)', width = 4)) %>%
-    add_trace(y = ~high_2007, name = 'High 2007', line = list(color = 'rgb(205, 12, 24)', width = 4, dash = 'dash')) %>%
-    add_trace(y = ~low_2007, name = 'Low 2007', line = list(color = 'rgb(22, 96, 167)', width = 4, dash = 'dash')) %>%
-    add_trace(y = ~high_2000, name = 'High 2000', line = list(color = 'rgb(205, 12, 24)', width = 4, dash = 'dot')) %>%
-    add_trace(y = ~low_2000, name = 'Low 2000', line = list(color = 'rgb(22, 96, 167)', width = 4, dash = 'dot')) %>%
-    layout(title = "Average High and Low Temperatures in New York",
-           xaxis = list(title = "Months"),
-           yaxis = list (title = "Temperature (degrees F)"))
+  selected_crime <- filter(dataset, Crime.Subcategory == crime) %>% 
+    select(Crime.Subcategory, n, month_in_year)
+  
+  p <- plot_ly(data = dataset, 
+               x = ~month_in_year, 
+               y = ~n, 
+               type = 'scatter', 
+               mode = 'lines'
+               ) %>%
+    layout(title = "Crimes Frequencies Across the Year",
+           xaxis = list(title = "Month (By Order in Year)"),
+           yaxis = list (title = "Number of Cases"))
 }
